767a2173ee075dbc4cb426b8c52cce919216c2d7
Abdulazeez5897 <137459978+Abdulazeez5897@users.noreply.github.com>
2025-06-02 11:14:10 +0100

    Day 2 tasks

---
+++ Day 2.dart
@@ -0,0 +1,32 @@
+//Task 1
+
+double calculateBMI(double weight, double height) {
+  double bmi = weight / (height * height);
+  return bmi;
+}
+
+void main() {
+  double myWeight = 68.0;
+  double myHeight = 1.53;
+
+  double myBMI = calculateBMI(myWeight, myHeight);
+  print('My BMI is ${myBMI.toStringAsFixed(1)}'); 
+}
+
+//Task 2
+
+int findGreaterThan(int a, int b) {
+  if(a > b){
+    return a;
+  }else{
+    return b;
+  }
+}
+
+void main(){
+  int c = 7;
+  int d = 5;
+  int greaterThan = findGreaterThan(c, d);
+  
+  print('The greater number is $greaterThan');
+}
