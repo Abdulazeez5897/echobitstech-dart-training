54f9a8d578ebf11865950724f133b70dcac0346d
Abdulazeez5897 <137459978+Abdulazeez5897@users.noreply.github.com>
2025-06-02 11:17:35 +0100

    Day 4 task

---
+++ Day 4.dart
@@ -0,0 +1,23 @@
+void main() {
+  String name = 'Abdulazeez';
+  int age = 23;
+  age--;
+
+  if (age < 0) {
+    print("Error: Age can’t be negative.");
+    return;
+  }
+
+  String message;
+  if (age < 13) {
+    message = "You’re a child.";
+  } else if (age < 18) {
+    message = "You’re a teenager.";
+  } else if (age < 60) {
+    message = "You’re an adult.";
+  } else {
+    message = "You’re a senior.";
+  }
+
+  print("Hello, $name! You are $age years old. $message");
+}
