10580826779575fc727059085d382bdaacef6071
Abdulazeez5897 <137459978+Abdulazeez5897@users.noreply.github.com>
2025-06-02 11:11:03 +0100

    Day 1 tasks

---
+++ Day 1.dart
@@ -0,0 +1,20 @@
+//Task 1
+
+void main() {
+  String name = 'Usman Abdulazeez';
+  int age = 24;
+  double weight = 80.0;
+  
+  print("My name is $name and I'm $age, I weigh $weight kg");
+}
+
+//Task 2
+
+void main() {
+  int lenght = 10;
+  int breath = 5;
+  
+  int area = lenght * breath;
+  
+  print('The are of rectangle is equal $area');
+}
